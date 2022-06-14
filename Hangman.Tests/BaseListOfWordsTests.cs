e9e252c5898390041fe0e7e3c4e39204920d87f9
Paula Marija Kuročkina <p.m.kurockina@gmail.com>
2022-06-14 18:39:17 +0300

    ToDo Unittestu papildināšana iesākta

---
+++ Hangman.Tests/BaseListOfWordsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Hangman.Tests
+//{
+//    public class BaseListOfWordsTests
+//    {
+
+//        public void LoadListOfWords_WhenCorrectStreamReader_ThenReturnsCorrectStreamReader(string tries, string result)
+//        {
+//            var response = BaseListOfWords.LoadListOfWords(tries);
+
+//            Xunit.Assert.Equal(result, (IEnumerable<char>)response);
+        
+//        }
+
+
+//    }
+//}
